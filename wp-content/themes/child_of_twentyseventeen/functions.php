9faa6d23df931ed6dc8fcf8a995b779fb2fb3a89
ces_jyrwa <ces.jyrwa@gmail.com>
2019-06-07 13:13:49 -0400

    changed background color and h1 and h2 fonts style and color

--- wp-content/themes/child_of_twentyseventeen/functions.php
+++ wp-content/themes/child_of_twentyseventeen/functions.php
@@ -11,3 +11,10 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function wpb_add_google_fonts() {wp_enqueue_style( 'wpb-google-fonts','https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif&display=swap', false ); }
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+?>
+
+
